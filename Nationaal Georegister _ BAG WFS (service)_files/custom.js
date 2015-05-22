4f92a3fa18c9f4fffd0d05312ff2c110f07a127a
Simeon Nedkov <simeon@ndkv.nl>
2015-05-22 10:45:11 +0200

    add ZeroClipboard

---
+++ Nationaal Georegister _ BAG WFS (service)_files/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	ZeroClipboard.config( { swfPath: "./Nationaal Georegister _ BAG WFS (service)_files/ZeroClipboard.swf" } );
+	var client = new ZeroClipboard( document.getElementById("copy-button") );
+
+	client.on( "ready", function( readyEvent ) {
+	  //alert( "ZeroClipboard SWF is ready!" );
+
+	  client.on( "aftercopy", function( event ) {
+	    // `this` === `client`
+	    // `event.target` === the element that was clicked
+	    // event.target.style.display = "none";
+	    // alert("Copied text to clipboard: " + event.data["text/plain"] );
+	  } );
+	} );
+});
+
